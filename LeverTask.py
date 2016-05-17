942e49b13dd94dadc67caca5deb53840c8f6fe8f
Spencer B <boyer2@uw.edu>
2016-05-17 16:47:20 +0000

    begin control loop

--- LeverTask.py
+++ LeverTask.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python
-from flask import Flask, render_template
-#from flask.ext.socketio import SocketIO, emit
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('index.html')
+import RPi.GPIO as GPIO
+import time
+import spidev #SPI 
 
-if __name__ == '__main__':
-	app.run('0.0.0.0')
+#state variables
+running = True
+
+#start
+def start():
+    main()
+
+# main loop for motor control etc
+def main():
+    while running:
+        [leftLever, rightLever] = readMCP3002(0)
+        print leftLever,
+        print rightLever
+        time.sleep(0.002)
+
+# function to read from the mcp3002
+def readMCP3002(device):
+     
+    spi = spidev.SpiDev(0,0)
+    spi.open(0,device)
+    msb = spi.xfer([0x70]) #ADC channel 1
+    lsb = spi.xfer([0])
+    adc1 = msb[0]<<8|lsb[0]
+    msb = spi.xfer([0x60]) #ADC channel 2
+    lsb = spi.xfer([0])
+    adc2 = msb[0]<<8|lsb[0]
+    spi.close()
+    return [adc1,adc2]
+               
+#call start and begin running
+if __name__ == "__main__"
+    start()
