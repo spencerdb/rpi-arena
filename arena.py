3eb042b1f6e79d2e6162a90e06e7cf1a09de9579
Spencer <boyer2@uw.edu>
2016-10-26 12:24:08 -0700

    updated arena class

--- arena.py
+++ arena.py
@@ -7,6 +7,16 @@ boyer2@uw.edu
 import time
 import numpy
 import re
+import servo.py
+
+try:
+    import spidev
+except:
+    class spi: #dummy class for running off of the raspberry pi
+        def __init__(self):
+            return 0
+
+
 try:
     import RPi.GPIO as GPIO
 except:
@@ -28,6 +38,23 @@ except:
             return 0
         def cleanup():
             return 0
+            
+#holds the data for one actuator
+class Actuator:
+    #min/max in ms
+    maxPoint = 2.0
+    minPoint = 1.0
+    
+    pin = -1
+    channel = -1
+    value = -1
+    setPoint = 1.5
+    def __init__(self, pin, channel):
+        self.pin = pin
+        self.channel = channel
+    def setPercent(percent):
+        setPoint = (percent / 100 + 1.0)
+    
     
 
 class Arena:
@@ -36,20 +63,21 @@ class Arena:
     self.HIGH = GPIO.HIGH
     self.LOW = GPIO.LOW
     
-    
+    def setActuator(n,s):
+        servo.setActuator(n,s)
         
     def digitalRead():
-        return 0
+        return -1
         
     def analogRead():
         return -1
         
     def digitalWrite(pin, value):
-        return 0
+        return -1
         
     
     def analogWrite():
-        return 0
+        return -1
     
     def setup(pin, mode):
         GPIO.setmode(GPIO.BCM)
@@ -58,34 +86,42 @@ class Arena:
     def __del__(self):
         GPIO.cleanup()
 
-class LeverArena(Arena):
+class LeverArena27(Arena):
     
-    hardware = 'Virtual'
+    hardware = '2.7'
         
     
     def __init(self):
-        hl0 = [5, 8]
-        vl0 = [6, 9]
-        hr0 = [12, 10]
-        vr0 = [13, 11]
-        hl1 = [16, 12]
-        vl1 = [19, 13]
-        vr1 = [20, 14]
-        hr1 = [26, 15]
+        
+        #actuators [pwm pin, adc channel]
+        hl0 = Actuator(5, 8)
+        vl0 = Actuator(6, 9)
+        hr0 = Actuator(12, 10)
+        vr0 = Actuator(13, 11)
+        hl1 = Actuator(16, 12)
+        vl1 = Actuator(19, 13)
+        vr1 = Actuator(20, 14)
+        hr1 = Actuator(26, 15)
         
         BNC0 = [2, 4]
         BNC1 = [3, 5]
         BNC2 = [14, 6]
         BNC3 = [4, 7]
+        
+        aux0 =
+        aux1 =
+        aux2 =
+        aux3 =
                 
         
     
     
     #dummy function to be replaced with spi protocol per hardware
-    def analogRead(pin):
-        if (type(pin) == list):
-            pin = pin[1]
+    def analogRead(channel):
+        if (type(channel) == list):
+            channel = channel[1]
         for val in range(0, 16):
             analog(val) = numpy.sin(time.time()*2*numpy.pi/5.0)
             
     def __del__(self):
+        GPIO.cleanup()
