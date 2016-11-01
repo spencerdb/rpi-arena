6a555effdaa788b341065733ca6dc2f8e047c814
Spencer Boyer <boyer2@uw.edu>
2016-11-01 18:02:38 +0000

    added support for mcp3208

--- arena.py
+++ arena.py
@@ -1,5 +1,5 @@
 """
-Arena base class for hardware abstraction of raspberry pi based training arena
+Control class for lever arena board revision 2.7
 Revision 1.0
 @author Spencer Boyer
 boyer2@uw.edu
@@ -7,84 +7,8 @@ boyer2@uw.edu
 import time
 import numpy
 import re
-import servo.py
-
-try:
-    import spidev
-except:
-    class spi: #dummy class for running off of the raspberry pi
-        def __init__(self):
-            return 0
-
-
-try:
-    import RPi.GPIO as GPIO
-except:
-    class GPIO: #empty class for testing code off the raspberry pi
-        IN = 1
-        OUT = 0
-        HIGH = 1
-        LOW = 0
-        BCM = 11
-        def setmode(mode):
-            return 0
-        def setup(pin, mode):
-            return 0
-        def output(val):
-            return 0
-        def input():
-            return 0
-        def PWM(pin, value):
-            return 0
-        def cleanup():
-            return 0
-            
-#holds the data for one actuator
-class Actuator:
-    #min/max in ms
-    maxPoint = 2.0
-    minPoint = 1.0
-    
-    pin = -1
-    channel = -1
-    value = -1
-    setPoint = 1.5
-    def __init__(self, pin, channel):
-        self.pin = pin
-        self.channel = channel
-    def setPercent(percent):
-        setPoint = (percent / 100 + 1.0)
-    
-    
-
-class Arena:
-    self.IN = GPIO.IN
-    self.OUT = GPIO.OUT
-    self.HIGH = GPIO.HIGH
-    self.LOW = GPIO.LOW
-    
-    def setActuator(n,s):
-        servo.setActuator(n,s)
-        
-    def digitalRead():
-        return -1
-        
-    def analogRead():
-        return -1
-        
-    def digitalWrite(pin, value):
-        return -1
-        
-    
-    def analogWrite():
-        return -1
-    
-    def setup(pin, mode):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pin,mode)
-        
-    def __del__(self):
-        GPIO.cleanup()
+import servo
+import RPi.GPIO as GPIO
 
 class LeverArena27(Arena):
     
@@ -92,36 +16,40 @@ class LeverArena27(Arena):
         
     
     def __init(self):
-        
+       	from mcp3208 import mcp3208 
+	adc0 = mcp3208(0)
+	adc1 = mcp3208(1)	
+	
         #actuators [pwm pin, adc channel]
-        hl0 = Actuator(5, 8)
-        vl0 = Actuator(6, 9)
-        hr0 = Actuator(12, 10)
-        vr0 = Actuator(13, 11)
-        hl1 = Actuator(16, 12)
-        vl1 = Actuator(19, 13)
-        vr1 = Actuator(20, 14)
-        hr1 = Actuator(26, 15)
+        HL0 = [5, 8]
+        VL0 = [6, 9]
+        HR0 = [12, 10]
+        VR0 = [13, 11]
+        HL1 = [16, 12]
+        VL1 = [19, 13]
+        VR1 = [20, 14]
+        HR1 = [26, 15]
         
+	#BNC [digital out pin, adc channel]
         BNC0 = [2, 4]
         BNC1 = [3, 5]
         BNC2 = [14, 6]
         BNC3 = [4, 7]
+	
+	#Sensors
+	LL0 = [2,3]
+	RL0 = [0,1]
+	LL1 = [4,5]
+	RL1 = [6,7]
         
-        aux0 =
-        aux1 =
-        aux2 =
-        aux3 =
-                
+	#auxilary digital IO 
+        aux0 = [24]
+        aux1 = [21]
+        aux2 = [23]
+        aux3 = [25]
+		
         
     
-    
-    #dummy function to be replaced with spi protocol per hardware
-    def analogRead(channel):
-        if (type(channel) == list):
-            channel = channel[1]
-        for val in range(0, 16):
-            analog(val) = numpy.sin(time.time()*2*numpy.pi/5.0)
             
     def __del__(self):
         GPIO.cleanup()
---
+++ mcp3208.py
@@ -0,0 +1,35 @@
+#~/usr/bin/python
+#Function for reading an MCP3208 ADC from a raspberry pi
+#Spencer Boyer boyer2@uw.edu University of Washington
+#Adapted from http://www.paulschow.com/2013/08/monitoring-temperatures-using-raspberry.html
+
+import spidev
+
+class mcp3208:
+
+	#initialize mcp3208 on chip select device
+	def __init__(self,device):
+		
+		self.values = [-1] * 8	
+		self.spi = spidev.SpiDev()
+		self.spi.open(0,device)
+	
+	#Read MCP3208 and return array of value of each channel
+	def readADC(self):
+		for channel in range(0,8):
+			self.values[channel] = self.readChannel(channel)
+		return self.values		
+
+	#Read single channel of MCP3208
+	def readChannel(self, channel):
+		if (channel > 7 or channel < 0):
+			return -1
+		readBytes = self.spi.xfer2([1, 8 + channel << 4, 0])
+		self.values[channel] = (((readBytes[1] & 3) << 8) + readBytes[2])
+		return self.values[channel]
+
+	#Close the spi device
+	def __del__(self):
+		self.spi.close()
+		return 0
+
